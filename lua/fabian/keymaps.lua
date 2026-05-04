484a865873c6273f4cb8e01cfc25724c6da6bc3a
Fabian665 <71074539+Fabian665@users.noreply.github.com>
2026-05-04 14:53:21 +0300

    Modified options
    Removed some options I set and I'm not sure I like.
    Changed description of keymaps.
    Removed out-of-place lsp related keymap.

--- lua/fabian/keymaps.lua
+++ lua/fabian/keymaps.lua
@@ -1,22 +1,20 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Start netwr" })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join with line below" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line and keep cursor" })
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up" })
-vim.keymap.set("n", "n", "nzz", { desc = "Next search" })
-vim.keymap.set("n", "N", "Nzz", { desc = "Prev search" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+vim.keymap.set("n", "n", "nzz", { desc = "Next search result and center" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Previous search result and center" })
 
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Move selection down" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without yanking replaced text" })
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Copy to sys clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy line to sys clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete to null" })
-vim.keymap.set({"n", "v"}, "<leader>c", [["_c]], { desc = "Replace to null" })
-
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action / Quick Fix" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "<leader>c", [["_c]], { desc = "Change without yanking" })
